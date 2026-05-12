05dc9ae25f4ba589ea0f9809f7ac89a3b6a235f4
SirJonasM <jonas.moewes02@gmail.com>
2026-05-12 15:15:01 +0200

    some code inmprovements

--- code/main_multithreaded.py
+++ code/main_multithreaded.py
@@ -11,21 +11,13 @@ from CameraServerClass import CameraServer
 from TRSensors import TRSensors
 from ServoControllerClass import ServoController
 from AlphaBot2 import AlphaBot2
+from default_values import * 
 
-# LED strip configuration constants:
-LED_COUNT      = 4      # Number of LED pixels.
-LED_PIN        = 18     # GPIO pin connected to the pixels (must support PWM!).
-LED_FREQ_HZ    = 800000 # LED signal frequency in hertz (usually 800khz)
-LED_DMA        = 5      # DMA channel to use for generating signal (try 5)
-LED_BRIGHTNESS = 255    # Set to 0 for darkest and 255 for brightest
-LED_INVERT     = False  # True to invert the signal (when using NPN transistor level shift)
-LED_CHANNEL    = 0
 
 # Global flag to shutdown
 stop_event = False
 
 
-CENTER = 2000  
 POWER_DIFF_MAX = 90
 
 
--- code/main_seq.py
+++ code/main_seq.py
@@ -1,33 +1,7 @@
 import RPi.GPIO as GPIO
-from rpi_ws281x import Adafruit_NeoPixel, Color
 import argparse
-import torch
-from torchvision import models, transforms
-from torchvision.models.quantization import MobileNet_V2_QuantizedWeights
-import ast
 import time
-from CameraServerClass import CameraServer
-from TRSensors import TRSensors
-from ServoControllerClass import ServoController
 from AlphaBot2 import AlphaBot2
-
-# LED strip configuration constants:
-LED_COUNT      = 4      # Number of LED pixels.
-LED_PIN        = 18     # GPIO pin connected to the pixels (must support PWM!).
-LED_FREQ_HZ    = 800000 # LED signal frequency in hertz (usually 800khz)
-LED_DMA        = 5      # DMA channel to use for generating signal (try 5)
-LED_BRIGHTNESS = 255    # Set to 0 for darkest and 255 for brightest
-LED_INVERT     = False  # True to invert the signal (when using NPN transistor level shift)
-LED_CHANNEL    = 0
-
-# Global flag to shutdown
-CENTER = 2000  # Sensor center value
-
-SHOE_INDICES   = {770, 774, 630 }   
-BOTTLE_INDICES = {440, 737, 898}         
-MUG_INDICES    = {504, 968}              
-
-
 
 def main(bot, not_only_line):
     stop_event = False
@@ -45,6 +19,7 @@ def main(bot, not_only_line):
 
     print("Min:", bot.tr_sensor.calibratedMin)
     print("Max:", bot.tr_sensor.calibratedMax)
+    print("Running with not_only_line: ", not_only_line)
 
     try:
         while not stop_event:
@@ -72,7 +47,7 @@ if __name__ == '__main__':
     parser.add_argument('--ki', type=float, default=0.001, help='Integral gain (default: 0.001)')
     parser.add_argument('--kd', type=float, default=1.5, help='Derivative gain (default: 1.5)')
     parser.add_argument('--speed', type=int, default=15, help='Base motor speed (default: 15)')
-    parser.add_argument('--not-only-line', type=bool, default=False, help='Base motor speed (default: 15)')
+    parser.add_argument('--not-only-line', type=bool, default=False, help='Activates Line recognition and object detection')
 
     args = parser.parse_args()
     bot = AlphaBot2(kp=args.kp, ki=args.ki, kd=args.kd, speed=args.speed)
